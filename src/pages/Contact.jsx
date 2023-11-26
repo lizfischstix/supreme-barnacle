1a4dc2a0a319785c10938f56867b5703e0aa47d2
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-11-25 20:57:06 -0600

    all functional bits up and running...on to beautification.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react';
+import { validateEmail } from '../utils';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+
 export default function Contact() {
+  const [name, setName] = useState ('');
+  const [email, setEmail] = useState ('');
+  const [message, setMessage] = useState ('');
+  const [errorMessage, setErrorMessage] = useState ('');
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+  const handleValidation = (e)=>{
+    if(e.target.name === 'email'){
+      if(!validateEmail(e.target.value)){
+        setErrorMessage('Invalid Email')
+      }else{
+        setErrorMessage('')
+      }
+    }else{
+      if(!e.target.value.length){
+        setErrorMessage('missing required field')
+      }else{
+        setErrorMessage('')
+      }
+
+    }
+  }
+  const handleSubmit = e=>{
+    e.preventDefault ();
+    setName('');
+    setEmail('');
+    setMessage('');
+    setErrorMessage('thanks for your message!')
+
+  }
   return (
-    <div>IN PROGRESS</div>
+    <Form>
+       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="your name here" name='name' value={name} onChange={handleInputChange} onBlur={handleValidation} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" name='email' value={email} onChange={handleInputChange} onBlur={handleValidation} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} name='message' value={message} onChange={handleInputChange} onBlur={handleValidation}  />
+      </Form.Group>
+      <Button disabled={!(name && email && message)|| errorMessage } onClick={handleSubmit}>Submit</Button>
+      {errorMessage && <p>{errorMessage}</p>}
+    </Form>
   );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,22 +1,99 @@
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {Container, Card, Col, Row, Button} from 'react-bootstrap';
+
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
+    <div className="App">  
+    <Container className='p-4'> 
+    <Row> 
+   <Col md="4">  
+   <Card>  
+   <Card.Img variant="top" src='src/assets/images/projectPics/GameOn.png' />  
+   <Card.Body>  
+     <Card.Title>Game On!</Card.Title>  
+     <Card.Text>  
+    A free online gaming ecosystem where users can rate the games they've played and save the ones they like.
+     </Card.Text>  
+   <div className='buttonLinks'> <a href="https://grisly-coffin-11200-fb183baf5ee2.herokuapp.com/"><Button variant="warning">Game On!</Button></a>
+    <a href="https://github.com/lizfischstix/GAME-ON"><Button variant="warning">on GitHub</Button></a></div>
+   </Card.Body>  
+ </Card>  
+     </Col>  
+     
+     <Col md="4">  
+   <Card>  
+   <Card.Img variant="top" src='src/assets/images/projectPics/TechBog.png' />  
+   <Card.Body>  
+     <Card.Title>Tech Blog</Card.Title>  
+     <Card.Text>  
+    Where users can create blog posts and comment on the posts of others.
+     </Card.Text>  
+     <div className='buttonLinks'> <a href="https://cuddly-happiness-874f92dec1e1.herokuapp.com/"><Button variant="warning">Tech Blog</Button></a>
+    <a href="https://github.com/lizfischstix/cuddly-happiness"><Button variant="warning">on GitHub</Button></a></div>
+   </Card.Body>  
+ </Card> 
+ </Col>  
+     <Col md="4">  
+   <Card>  
+   <Card.Img variant="top" src='src/assets/images/projectPics/textEditor.png' />  
+   <Card.Body>  
+     <Card.Title>Just Another Text Editor</Card.Title>  
+     <Card.Text>  
+    A progressive web application for writing and saving notes.
+     </Card.Text>  
+     <div className='buttonLinks'> <a href="https://effective-winner-485806fb345e.herokuapp.com/"><Button variant="warning">JATE</Button></a>
+    <a href=""><Button variant="warning">on GitHub</Button></a></div>
+   </Card.Body>  
+ </Card>  
+     </Col>  
+     </Row>
+     <Row> 
+   <Col md="4">  
+   <Card>  
+   <Card.Img variant="top" src='src/assets/images/projectPics/GameOn.png' />  
+   <Card.Body>  
+     <Card.Title>Game On!</Card.Title>  
+     <Card.Text>  
+    A free online gaming ecosystem where users can rate the games they've played and save the ones they like.
+     </Card.Text>  
+   <div className='buttonLinks'> <a href="https://grisly-coffin-11200-fb183baf5ee2.herokuapp.com/"><Button variant="warning">Game On!</Button></a>
+    <a href="https://github.com/lizfischstix/GAME-ON"><Button variant="warning">on GitHub</Button></a></div>
+   </Card.Body>  
+ </Card>  
+     </Col>  
+     
+     <Col md="4">  
+   <Card>  
+   <Card.Img variant="top" src='src/assets/images/projectPics/TechBog.png' />  
+   <Card.Body>  
+     <Card.Title>Tech Blog</Card.Title>  
+     <Card.Text>  
+    Where users can create blog posts and comment on the posts of others.
+     </Card.Text>  
+     <div className='buttonLinks'> <a href="https://cuddly-happiness-874f92dec1e1.herokuapp.com/"><Button variant="warning">Tech Blog</Button></a>
+    <a href="https://github.com/lizfischstix/cuddly-happiness"><Button variant="warning">on GitHub</Button></a></div>
+   </Card.Body>  
+ </Card> 
+ </Col>  
+     <Col md="4">  
+   <Card>  
+   <Card.Img variant="top" src='src/assets/images/projectPics/textEditor.png' />  
+   <Card.Body>  
+     <Card.Title>Just Another Text Editor</Card.Title>  
+     <Card.Text>  
+    A progressive web application for writing and saving notes.
+     </Card.Text>  
+     <div className='buttonLinks'> <a href="https://effective-winner-485806fb345e.herokuapp.com/"><Button variant="warning">JATE</Button></a>
+    <a href=""><Button variant="warning">on GitHub</Button></a></div>
+   </Card.Body>  
+ </Card>  
+     </Col>  
+     </Row>
+
+ </Container>  
+     </div>
+     
+     
   );
+    
 }
