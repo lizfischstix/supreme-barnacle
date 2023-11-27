0a097c598c63a793d18fe2fc2f28c78e5384de65
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-11-27 09:35:44 -0600

    changed to reactbootstrop on hero.  Trying to figure why some images aren rendering.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 
 function Hero() {
   const currentPage = useLocation().pathname;
   return (
-    <div className='container-fluid hero'>
-      <div className='row pl-5'>
-        <div className='col-sm-9 left'> 
-        <div className='name'>Liz Fischnich</div>
-          <div className='name-tag pl-6'>Hello World!</div>
-        </div>
-     </div>
-    </div>
+    <Container fluid className='hero'>
+      <Row className='pl-5'>
+        <Col Text-start> 
+        <Col className='name'>Liz Fischnich</Col>
+          <Row className='name-tag pl-6'>Hello World!</Row>
+        </Col>
+     </Row>
+    </Container>
       );
 }
 
