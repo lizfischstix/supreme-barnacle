4d571a209de3802eec4ad743c6788e38f801fe2f
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-11-27 21:06:05 -0600

    oops. fixed padding in header.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   const currentPage = useLocation().pathname;
   return (
     <Container fluid className='px-5 hero'>
-      <Row className='ml-4'>
+      <Row className='pl-4'>
         <Col Text-start> 
         <Col className='name'>Liz Fischnich</Col>
           <Row className='name-tag'>Hello World!</Row>
